5e0f4ee641d0bec5efd0a0f812a01584b1113637
gcharasson <gabin.charasson@etu.u-bordeaux.fr>
2024-05-06 09:20:31 +0200

    feat(): add navigation to map page

--- lib/app/widgets/shared/navbar.dart
+++ lib/app/widgets/shared/navbar.dart
@@ -1,6 +1,24 @@
 import 'package:flutter/material.dart';
 
-class BottomNavBar extends StatelessWidget {
+class BottomNavBar extends StatefulWidget {
+  final Function(int) onItemTapped;
+
+  BottomNavBar({required this.onItemTapped});
+
+  @override
+  _BottomNavBarState createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+    widget.onItemTapped(index); // Call the callback method
+  }
+
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
@@ -8,6 +26,7 @@ class BottomNavBar extends StatelessWidget {
       elevation: 0,
       selectedItemColor: const Color.fromRGBO(23, 23, 25, 1),
       unselectedItemColor: const Color.fromRGBO(43, 44, 43, 1),
+      currentIndex: _selectedIndex,
       showSelectedLabels: false,
       items: <BottomNavigationBarItem>[
         BottomNavigationBarItem(
@@ -39,7 +58,6 @@ class BottomNavBar extends StatelessWidget {
           label: '',
         ),
       ],
-      // Add callback functions to handle navigation if necessary
     );
   }
 }
